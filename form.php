18e6df58f83fba42b0e9500fda6bd488f2e1fe70
Richhcardo <170968030+Richhcardo@users.noreply.github.com>
2024-05-29 06:56:50 -0400

    update
    Files update to php

---
+++ form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Formulaire d'invitation pour la fête.">
+    <meta name="author" content="Ton Nom">
+    <title>Formulaire d'invitation</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Formulaire d'invitation</h1>
+    <form action="submit_form.php" method="post">
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="name" required>
+        <input type="submit" value="Envoyer">
+    </form>
+</body>
+</html>
